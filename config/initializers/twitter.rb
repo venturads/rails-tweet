5763adfeb383d914da88bfc228fe412ef33b5be9
Albert Ventura <albert@venturads.com>
2017-09-07 22:04:35 +0000

    update config

--- config/initializers/twitter.rb
+++ config/initializers/twitter.rb
@@ -1,6 +1,6 @@
 $twitter = Twitter::REST::Client.new do |config|
-  config.consumer_key = ENV['Q1vBqGRQWENac1CCIKSTvmJzn']
-  config.consumer_secret = ENV['YVa8K05ZjTSyk07NnpqyglfnXYEB0r8sAcLRlDxb6LqOyvXkLk']
-  config.access_token = ENV['90027013-P0WhYlFiGBrU7ua5T7yhCAf2GY1md9CvICTvjhUOb']
-  config.access_token_secret = ENV['cw2KnEPdWmZrSZtL7fjpJinpDcTFaAbk5YcRHhNfVvi3l']
+  config.consumer_key = ENV['CONSUMER_KEY']
+  config.consumer_secret = ENV['CONSUMER_SECRET']
+  config.access_token = ENV['YOUR_ACCESS_TOKEN']
+  config.access_token_secret = ENV['YOUR_ACCESS_SECRET']
 end
